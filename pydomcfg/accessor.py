76b1722d6d5cbef7fef9ae71f0f7edc54cb6bf67
malmans2 <mattia.almansi@jhu.edu>
2021-06-24 16:32:18 +0100

    Untested, but the main implementation should be in good shape

--- pydomcfg/accessor.py
+++ pydomcfg/accessor.py
@@ -1,4 +1,6 @@
+import inspect
 import warnings
+from collections import ChainMap
 from pathlib import Path
 from typing import IO, Any, Callable, TypeVar, Union, cast
 
@@ -38,7 +40,7 @@ class Accessor:
     def __init__(self, xarray_obj: Dataset):
         self._obj = xarray_obj
         self._jpk = 0
-        self._nml_ref = None
+        self._nml_ref_path = ""
 
     # Set attributes
     @property
@@ -67,7 +69,7 @@ class Accessor:
         -------
         str
         """
-        return self.nml_ref_path
+        return self._nml_ref_path
 
     @nml_ref_path.setter
     def nml_ref_path(self, value: str):
@@ -86,26 +88,33 @@ class Accessor:
         TODO
         """
 
-        # Sanity checks
-        self._namelist_checks
+        # Parse and return a ChainMap
+        nml = self._namelist_parser(nml_cfg_path_or_io)
 
-        # Read namelists: cfg overrides ref
-        # TODO:
-        #   If we specify the tird argument, the resulting namelist is written
-        #   The advantage is that it is formatted as the reference namelist
-        #   We could to it to a tmp file, then add the resulting string to the
-        #   global attributes of the final dataset.
-        nml = f90nml.patch(self.nml_ref_path, f90nml.read(nml_cfg_path_or_io))
+        # Pick zgr class
+        if nml["ln_zco"]:
+            zgr_class = Zco
+        else:
+            raise NotImplementedError("Only `Zco` has been implemented so far.")
 
-    @property
-    def _namelist_checks(self):
+        # Inspect the __call__ signatures and get the approriate variables
+        # from the namelists.
+        parameters = inspect.signature(zgr_class.__call__).parameters
+        kwargs = {key: nml[key] for key in parameters if key != "self"}
+        return Zco(self._obj, nml["jpkdta"])(**kwargs)
+
+    def _namelist_parser(
+        self, nml_cfg_path_or_io: Union[str, Path, IO[str]]
+    ) -> ChainMap:
 
         if not HAS_F90NML:
-            raise ImportError("`f90nml` MUST be installed to use `obj.from_namelist()`")
+            raise ImportError(
+                "`f90nml` MUST be installed" " to use `obj.domcfg.from_namelist()`"
+            )
 
         if not self.nml_ref_path:
             raise ValueError(
-                "Set `nml_ref_path` before calling `obj.from_namelist`"
+                "Set `nml_ref_path` before calling `obj.domcfg.from_namelist()`"
                 " For example: obj.domcfg.nml_ref_path = 'path/to/nml_ref'"
             )
 
@@ -113,3 +122,31 @@ class Accessor:
             warnings.warn(
                 "`obj.domcfg.jpk` is ignored." " `jpk` is inferred from the namelists."
             )
+
+        # Read namelists: cfg overrides ref
+        # TODO:
+        #   If we specify the third argument, the resulting namelist is written
+        #   The advantage is that it is formatted as the reference namelist
+        #   We could do it to a tmp file, then add the resulting string to the
+        #   global attributes of the final dataset.
+        nml_cfg = f90nml.read(nml_cfg_path_or_io)
+        nml = f90nml.patch(self.nml_ref_path, nml_cfg)
+
+        # Chain all nam blocks
+        # TODO:
+        #   Using ChainMap we disregard nam blocks, so assume there aren't
+        #   variables with the same name in different nam blocks. Is it OK?
+        #   If NO, maybe it's OK if we just select relevant blocks?
+        #   ChainMap would make life much easier...
+        #   We just look at the zgr call signatures, and we extract the variables
+        #   with the same name from any block.
+        chained = ChainMap(*nml.todict().values())
+
+        mutually_exclusive = ("ln_zco", "ln_zps", "ln_sco")
+        if sum(chained.get(key) for key in mutually_exclusive) != 1:
+            raise ValueError(
+                "One and only one of the following variables MUST be set:"
+                f" {mutually_exclusive}"
+            )
+
+        return chained
