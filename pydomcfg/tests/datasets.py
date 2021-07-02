6ce1fdd6aa791437a2ffd4237cad7db68b59330c
malmans2 <mattia.almansi@jhu.edu>
2021-07-02 17:31:26 +0100

    renaming

--- pydomcfg/tests/datasets.py
+++ pydomcfg/tests/datasets.py
@@ -2,9 +2,11 @@ import xarray as xr
 
 import pooch
 
-bathymeter_mo_ps43 = xr.open_dataset(
+BASE_URL = "https://github.com/pyNEMO/sample-datasets/raw/main"
+
+ds_amm7 = xr.open_dataset(
     pooch.retrieve(
-        url="https://github.com/pyNEMO/sample-datasets/raw/main/bathymeter_mo_ps43.nc",
+        url="/".join([BASE_URL, "bathymeter_amm7_mo_ps43.nc"]),
         known_hash="c8e6b6e185bc15e89e19a860acc79ed9fee335de0c9113af76dac82438297b45",
     )
 )
